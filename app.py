c667e9efcd95c73576da219f071da120f4a8d3ae
rez13 <cist.b220.y.katou@gmail.com>
2022-07-22 02:06:45 +0900

    出力結果と画像データの紐付け

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import json
 import torch
 import numpy as np
 from ai import Model
+import pandas as pd
+
 app = Flask(__name__)
 
 model = Model()
@@ -30,9 +32,8 @@ def aws_test():
     img = BytesIO(img)
 
     rekognition = RekognitionImage(img.getvalue())
+    # json_data に rekogのresponseが全部入ってる
     json_data = rekognition.detect_faces()
-
-    print(json_data)
 
     json_data = json_data['FaceDetails']
     json_data = json_data[0]['Landmarks']
@@ -44,27 +45,31 @@ def aws_test():
         x_list.append(x)
         x_list.append(y)
 
+    # input_dataをjson形式からtensor型に変換する
     x = np.array(x_list)
     x = torch.tensor(x, dtype=torch.float32)
 
-    # input_data に rekogのresponseが全部入ってる
-    # input_dataをjson形式からtensor型に変換する
-
     """ input_dataを推論器に渡して、出力(画像を返す) """
     output = detect_ukiyoe_face(x)
+
     print(output)
+    # 出力結果に対応する浮世絵顔を用意
+    labels = pd.read_csv('resources/label_data.csv')
+    filename = labels['singleface_filename'].values[output]
+    path = 'resources/arc_extracted_face_images/' + str(filename)
+    print(filename)
+    print(path)
+    ukiyoe_img = Image.open(path)
 
-    response = {"base64": output}
+    # 画像をjsonに変換
+    buffered = BytesIO()
+    ukiyoe_img.save(buffered, format="JPEG")
+    ukiyoe_byte = buffered.getvalue()
+    ukiyoe_base64 = base64.b64encode(ukiyoe_byte)
+    ukiyoe_str = ukiyoe_base64.decode('utf-8')
+
+    response = {"base64": ukiyoe_str}
     return jsonify(response)
-
-
-def detect_ukiyoe_face(x):
-
-    model.load_state_dict(torch.load('resources/model.pth'))
-    y = model(x)
-    y_label = torch.argmax(y, dim=0)
-    print(y_label)
-    return y_label.item()
 
 
 @app.route("/reverse", methods=["POST"])
@@ -94,6 +99,13 @@ def parse():
     response = {'base64': img_str}
 
     return jsonify(response)
+
+
+def detect_ukiyoe_face(x):
+    model.load_state_dict(torch.load('resources/model.pth'))
+    y = model(x)
+    y_label = torch.argmax(y, dim=0)
+    return y_label.item()
 
 
 if __name__ == '__main__':
